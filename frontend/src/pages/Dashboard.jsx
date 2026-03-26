52a3adec0e5db5861f01f77938f3a37c12c97b1f
keenanmacneill <macneill.keenan@gmail.com>
2026-03-26 15:15:40 -0400

    creates and styles repor submit on dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -5,13 +5,73 @@ export default function Dashboard() {
   return (
     <>
       <Header />
-      <div>TO DO: DASHBOARD, SUBMIT REPORT</div>;
+      <div className="dashboard-header-container">
+        <div className="dashboard-title-container">
+          <div className="dashboard-header-title">Dashboard</div>
+          <div className="dashboard-header-subtitle">
+            {/* TO DO: sync refresh with last refresh */}
+            AO: Pineland - last sync 4 min ago
+          </div>
+        </div>
+        <div className="dashboard-utility-container">
+          <button className="export-button">Export</button>
+          <button className="new-report-button">+ New Report</button>
+        </div>
+      </div>
+      <div className="dashboard-metrics-container">
+        {/* TO DO: insert metrics components here */}
+      </div>
+      <div className="dashboard-main-container">
+        <div className="dashboard-map">Report distribution</div>
+        <div className="dashboard-report-container">
+          <div className="dashboard-report-title">New report</div>
+          <div className="dashboard-report-body">
+            <div className="report-title-title">Title</div>
+            <input
+              className="report-title"
+              type="text"
+              placeholder="Brief descriptive title..."
+            ></input>
+            <div className="report-summary-title">Summary</div>
+            <input
+              className="report-summary"
+              type="text"
+              placeholder="Concise summary..."
+            ></input>
+            <div className="report-mgrs-title">MGRS</div>
+            <input
+              className="report-mgrs"
+              type="text"
+              placeholder="MGRS"
+            ></input>
+            <div className="report-mgrs-title">Latitude, longitude</div>
+            <input
+              className="report-lat-long"
+              type="text"
+              placeholder="Latitude, longitude"
+            ></input>
+            <div className="report-recommendations-title">Recommendations</div>
+            <input
+              className="report-recommendations"
+              type="text"
+              placeholder="Concise recommendations..."
+            ></input>
+            <div className="report-category-title">Category</div>
+            <select className="report-category">
+              {/* TO DO: generate options from categories table, multi-select */}
+            </select>
+            <div className="report-priority-title">Priority</div>
+            <select className="report-priority">
+              {/* TO DO: generate options from priority table */}
+            </select>
+          </div>
+          <button className="report-submit-button">Submit report</button>
+        </div>
+      </div>
     </>
   );
 }
 
-// Start with the report submission form.
-// It needs a lat/lng input (typed coordinates are fine for now), a free-text summary field, and a way to select one or more category tags.
 // Wire the form to your POST endpoint.
 // Handle the response, and show the user something meaningful whether it succeeds or fails.
 // Add a Leaflet map to the page.
