8d08fec6e2b661f31686b940ba775ec747d80add
keenanmacneill <macneill.keenan@gmail.com>
2026-03-30 18:37:18 -0400

    updates production

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -40,11 +40,8 @@ module.exports = {
   production: {
     client: 'pg',
     connection: {
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
     },
     migrations: {
       directory: './src/db/migrations',
