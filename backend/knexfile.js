d22afc887dcc393ba68757f4851fefbe33124a0a
keenanmacneill <macneill.keenan@gmail.com>
2026-03-30 18:21:02 -0400

    moves vite to dependencies for vercel deploy

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -19,4 +19,38 @@ module.exports = {
       directory: './src/db/seeds',
     },
   },
+
+  test: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    },
+    migrations: {
+      directory: './src/db/migrations',
+    },
+    seeds: {
+      directory: './src/db/seeds',
+    },
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    },
+    migrations: {
+      directory: './src/db/migrations',
+    },
+    seeds: {
+      directory: './src/db/seeds',
+    },
+  },
 };
