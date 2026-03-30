0ed48396f609787084f4fda1c70a2430237bd6af
keenanmacneill <macneill.keenan@gmail.com>
2026-03-30 13:45:44 -0400

    normalizes category in service

--- backend/src/api/services/categories-service.js
+++ backend/src/api/services/categories-service.js
@@ -5,7 +5,8 @@ exports.getAllCategories = async () => {
 };
 
 exports.createCategory = async category => {
-  const match = await categoriesModel.getCategoryName(category);
+  const normalizedCategory = category.trim().toLowerCase();
+  const match = await categoriesModel.getCategoryName(normalizedCategory);
 
   if (match) {
     const error = new Error('Category already exists.');
@@ -13,13 +14,16 @@ exports.createCategory = async category => {
     throw error;
   }
 
-  const [newCategory] = await categoriesModel.createCategory(category);
+  const [newCategory] =
+    await categoriesModel.createCategory(normalizedCategory);
 
   return newCategory;
 };
 
 exports.updateCategory = async (oldCategory, newCategory) => {
-  const match = await categoriesModel.getCategoryName(oldCategory);
+  const normalizedOldCategory = oldCategory.trim().toLowerCase();
+  const normalizedNewCategory = newCategory.trim().toLowerCase();
+  const match = await categoriesModel.getCategoryName(normalizedOldCategory);
 
   if (!match) {
     const error = new Error('Category does not exist.');
@@ -27,7 +31,7 @@ exports.updateCategory = async (oldCategory, newCategory) => {
     throw error;
   }
 
-  if (match.category === newCategory) {
+  if (match.category === normalizedNewCategory) {
     const error = new Error('No changes detected.');
     error.status = 400;
     throw error;
@@ -36,7 +40,7 @@ exports.updateCategory = async (oldCategory, newCategory) => {
   const categories = await categoriesModel.getAllCategories();
 
   for (let obj of categories) {
-    if (obj.category === newCategory.trim().toLowerCase()) {
+    if (obj.category === normalizedNewCategory) {
       const error = new Error('Category already exists.');
       error.status = 400;
       throw error;
@@ -44,15 +48,16 @@ exports.updateCategory = async (oldCategory, newCategory) => {
   }
 
   const [updatedCategory] = await categoriesModel.updateCategory(
-    oldCategory,
-    newCategory,
+    normalizedOldCategory,
+    normalizedNewCategory,
   );
 
   return updatedCategory;
 };
 
 exports.deleteCategory = async category => {
-  const match = await categoriesModel.getCategoryName(category);
+  const normalizedCategory = category.trim().toLowerCase();
+  const match = await categoriesModel.getCategoryName(normalizedCategory);
 
   if (!match) {
     const error = new Error('Category does not exist.');
@@ -60,7 +65,8 @@ exports.deleteCategory = async category => {
     throw error;
   }
 
-  const [deletedCategory] = await categoriesModel.deleteCategory(category);
+  const [deletedCategory] =
+    await categoriesModel.deleteCategory(normalizedCategory);
 
   return deletedCategory;
 };
