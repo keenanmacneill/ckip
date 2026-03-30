00d19d0c7ea1c62117271ddca9e79fd3184923a5
keenanmacneill <macneill.keenan@gmail.com>
2026-03-30 19:01:08 -0400

    updates cors to api url

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const API_URL = import.meta.env.VITE_API_URL;
 
 const app = express();
 
@@ -8,7 +9,7 @@ app.use(express.json());
 app.use(cookieParser(process.env.JWT));
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: API_URL,
     credentials: true,
   }),
 );
