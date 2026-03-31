8ac92da71f8cc2f3767447f51842c5b33f265f25
keenanmacneill <macneill.keenan@gmail.com>
2026-03-31 11:48:44 -0400

    adjust to 250 user and 500 reports

--- backend/src/db/seeds/02_reports.js
+++ backend/src/db/seeds/02_reports.js
@@ -3,5 +3,5 @@ const { generateReports } = require('../helpers/generateReports');
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex('reports').del();
-  await knex('reports').insert(await generateReports(knex, 1000));
+  await knex('reports').insert(await generateReports(knex, 500));
 };
