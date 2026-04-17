26a16d79c5cb0d9cbf3014d8169408601bec10a9
keenanmacneill <macneill.keenan@gmail.com>
2026-04-17 18:20:13 -0400

    updates CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,7 +18,7 @@ app.use(
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
-        callback(new Error('Not allowed by CORS'));
+        callback(null, false);
       }
     },
     credentials: true,
